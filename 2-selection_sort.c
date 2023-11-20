7eeb522f4ecdd5b09c83b9c28a2be2eddcf40cb3
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-11-20 18:32:58 +0000

    final

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -27,13 +27,13 @@ void selection_sort(int *array, size_t size)
 	int *min;
 	size_t i, j;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size < value_2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - value_1; i++)
 	{
 		min = array + i;
-		for (j = i + 1; j < size; j++)
+		for (j = i + value_1; j < size; j++)
 			min = (array[j] < *min) ? (array + j) : min;
 
 		if ((array + i) != min)
