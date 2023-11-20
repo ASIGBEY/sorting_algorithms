953f4c2ff18b81c2961a8418ca4fb3aaf1d03d03
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-11-20 16:39:12 +0000

    first

--- sort.h
+++ sort.h
@@ -35,17 +35,17 @@ typedef enum bool
 } bool;
 
 /**
- * struct listint_s - Doubly linked list node
+ * struct listint_t - Doubly linked list node
  *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-typedef struct listint_s
+typedef struct listint_t
 {
 	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+	struct listint_t *prev;
+	struct listint_t *next;
 } listint_t;
 
 /* Printing helper functions */
